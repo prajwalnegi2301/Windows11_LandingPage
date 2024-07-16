92289cb1cc769903a2f7e4d74de070cd5e4a8967
Prajwal Negi <166615651+prajwalnegi2301@users.noreply.github.com>
2024-07-16 23:45:16 +0530

    PractisingJS

---
+++ script.js
@@ -0,0 +1,13 @@
+taskbar=document.getElementsByClassName("taskbar")[0];
+startmenu=document.getElementsByClassName("startmenu")[0];
+
+taskbar.addEventListener("click",()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px";
+    }
+}
+)
